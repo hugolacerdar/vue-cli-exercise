bd43a6af567aa05998c7406e7c1f9d4847006434
Hugo <hugolacerdar@gmail.com>
2022-01-06 17:07:51 -0300

    Complete

---
+++ src/components/CityCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="place">
+    <img :src="image" width="235" height="300" />
+    <h2>{{ name }}</h2>
+    <p>{{ description }}</p>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CityCard",
+  props: {
+    name: String,
+    image: String,
+    description: String,
+  },
+};
+</script>
